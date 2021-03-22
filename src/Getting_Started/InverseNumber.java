96073621ad2946f7f33a98b4a6ddfefaae7d7638
UMESHvaidya <vaidyaumesh36@gmail.com>
2021-03-22 22:08:30 +0530

    Inverse Of A Number

---
+++ src/Getting_Started/InverseNumber.java
@@ -0,0 +1,16 @@
+package Getting_Started;
+
+import java.util.*;
+import java.lang.*;
+public class InverseNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String number = scn.next();
+        int []arr = new int[number.length()];
+        for (int i = number.length() - 1; i >=0 ; --i)
+            arr[(int)(number.charAt(i) - (int)'0'- 1)] = number.length() - i;
+        for(int i = arr.length -1; i >= 0; --i)
+            System.out.print(arr[i]);
+        System.out.println();
+    }
+}
